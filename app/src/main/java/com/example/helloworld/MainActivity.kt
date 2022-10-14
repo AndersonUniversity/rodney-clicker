7936d3df367fbfdd097a473e5a6a7d23f8c10e2c
jgalford <jalford@zincpartners.us>
2022-10-14 10:54:05 -0400

    start of save data

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.helloworld
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadData()
+
+        //TODO change the button id
+        findViewById<View>(R.id.button).setOnClickListener {
+            saveData()
+        }
     }
 
 //    /** Called when the user taps the Send button */ //TODO change this to shop page
@@ -26,5 +34,28 @@ class MainActivity : AppCompatActivity() {
         val ravenDollars = findViewById<EditText>(R.id.ravenDollars)
         val currNum = ravenDollars.text.toString().toInt()
         ravenDollars.setText((currNum + 1).toString())
+    }
+
+    /** Save Data */
+    private fun saveData() {
+        val currNum = findViewById<EditText>(R.id.ravenDollars).text.toString()
+
+        val sharedPref = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.apply {
+            putString("Raven_Dollars", currNum)
+        }.apply()
+
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
+    }
+
+    /** Load Data */
+    private fun loadData() {
+        val sharedPref = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedNum = sharedPref.getString("Raven_Dollars", null)
+
+        if (savedNum != null) {
+            findViewById<EditText>(R.id.ravenDollars).setText(savedNum)
+        }
     }
 }
