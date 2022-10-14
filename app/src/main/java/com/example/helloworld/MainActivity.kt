d66864f6330ef43828d87d25d8b77829ee4c4dbc
Michael Carroll <mrcarroll@anderson.edu>
2022-10-14 10:45:26 -0400

    added per second ticker

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.helloworld
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -11,15 +14,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-//    /** Called when the user taps the Send button */ //TODO change this to shop page
-//    fun sendMessage(view: View) {
-//        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
-//        val message = editText.text.toString()
-//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-//            putExtra(EXTRA_MESSAGE, message)
-//        }
-//        startActivity(intent)
-//    }
+    class AutoClicker(var dps: Int, var cost: Double, var numOwned: Int) {
+        fun buy() {
+            numOwned += 1
+            cost *= 1.5
+        }
+    }
+
+    private val rodney = AutoClicker(1, 10.0, 0)
+
+    //    /** Called when the user taps the Send button */ //TODO change this to shop page
+    //    fun sendMessage(view: View) {
+    //        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+    //        val message = editText.text.toString()
+    //        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+    //            putExtra(EXTRA_MESSAGE, message)
+    //        }
+    //        startActivity(intent)
+    //    }
 
     /** Called when the user taps the rodney button */
     fun increaseNum(view: View) {
@@ -27,4 +39,30 @@ class MainActivity : AppCompatActivity() {
         val currNum = ravenDollars.text.toString().toInt()
         ravenDollars.setText((currNum + 1).toString())
     }
+
+    fun addClicker(view: View) {
+        val ravenDollars = findViewById<EditText>(R.id.ravenDollars)
+        val currNum = ravenDollars.text.toString().toInt()
+        if (currNum >= rodney.cost) {
+            ravenDollars.setText((currNum - rodney.cost).toString())
+            rodney.buy()
+        }
+        addFunds(this)
+    }
+
+    //run every 60th of a second
+    fun addFunds(view: MainActivity) {
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val ravenDollars = findViewById<EditText>(R.id.ravenDollars)
+                val currNum = ravenDollars.text.toString().toInt()
+                ravenDollars.setText((currNum + rodney.dps).toString())
+                handler.postDelayed(this, 1000)
+            }
+        }, 1000)
+    }
 }
+
+
+
