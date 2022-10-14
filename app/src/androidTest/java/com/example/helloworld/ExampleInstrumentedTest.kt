f8f1fc7fb9759523d8a61dd0171f624b565c33c2
Jon Craton <jncraton@gmail.com>
2022-10-14 12:54:19 -0400

    Break instrumentation test

--- app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.helloworld", appContext.packageName)
+        assertEquals("com.example.helloworld!", appContext.packageName)
     }
 }
