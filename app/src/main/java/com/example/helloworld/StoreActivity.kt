582cf33368b7879441e73f8a5718683be07870d4
emmalee <emmalee.paarlberg@gmail.com>
2022-10-16 17:12:48 -0400

    adds button to navigate to store page

---
+++ app/src/main/java/com/example/helloworld/StoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.upgrade_layout)
+    }
+}
