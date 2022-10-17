76031602ce3dfbae36ed4c67fc1dd5bd333cc0ad
Jon Craton <jncraton@gmail.com>
2022-10-17 12:18:00 -0400

    Remove second click event registration (#13)
    This change removes a second click event registration requiring a user
    to press a button twice before successfully performing the button
    action. This fixes #12.

--- app/src/main/java/com/example/helloworld/StoreActivity.kt
+++ app/src/main/java/com/example/helloworld/StoreActivity.kt
@@ -3,7 +3,6 @@ package com.example.helloworld
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StoreActivity : AppCompatActivity() {
@@ -11,12 +10,10 @@ class StoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.upgrade_layout)
     }
-    /** Called when the user taps the Shop button */ // TODO change this to shop page
+
+    /** Called when the user taps the Home button */
     fun openHomePage(view: View) {
-        val buttonClick = findViewById<Button>(R.id.home_button)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            finish()
-        }
+        Intent(this, MainActivity::class.java)
+        finish()
     }
 }
