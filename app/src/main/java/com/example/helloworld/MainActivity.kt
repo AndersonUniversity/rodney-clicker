c7dcfd231bde24803ed878c7c77c6850b4f36d55
Michael Carroll <mrcarroll@anderson.edu>
2022-10-17 10:21:08 -0400

    added per second ticker

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,21 +7,25 @@ import android.os.Looper
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFunds(this)
     }
 
-    class AutoClicker(var dps: Int, var cost: Double, var numOwned: Int) {
+    class AutoClicker(var dps: Int, var cost: Int, var numOwned: Int) {
         fun buy() {
+           var multiplier = 1.5
             numOwned += 1
-            cost *= 1.5
+            cost = (cost * multiplier).toInt()
         }
     }
 
-    private val rodney = AutoClicker(1, 10.0, 0)
+    val rodney = AutoClicker(1, 10, 0)
+
 
     //    /** Called when the user taps the Send button */ //TODO change this to shop page
     //    fun sendMessage(view: View) {
@@ -47,22 +51,18 @@ class MainActivity : AppCompatActivity() {
             ravenDollars.setText((currNum - rodney.cost).toString())
             rodney.buy()
         }
-        addFunds(this)
     }
 
-    //run every 60th of a second
+
     fun addFunds(view: MainActivity) {
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(object : Runnable {
             override fun run() {
                 val ravenDollars = findViewById<EditText>(R.id.ravenDollars)
                 val currNum = ravenDollars.text.toString().toInt()
-                ravenDollars.setText((currNum + rodney.dps).toString())
+                ravenDollars.setText((currNum + (rodney.dps * rodney.numOwned)).toString())
                 handler.postDelayed(this, 1000)
             }
         }, 1000)
     }
 }
-
-
-
