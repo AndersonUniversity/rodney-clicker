47ed3023d52ead3fcea19af9e87bf3475faed594
emmalee <emmalee.paarlberg@gmail.com>
2022-10-17 09:57:49 -0400

    navigate back to home page

--- app/src/main/java/com/example/helloworld/StoreActivity.kt
+++ app/src/main/java/com/example/helloworld/StoreActivity.kt
@@ -1,11 +1,22 @@
 package com.example.helloworld
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.upgrade_layout)
+    }
+    /** Called when the user taps the Shop button */ // TODO change this to shop page
+    fun openHomePage(view: View) {
+        val buttonClick = findViewById<Button>(R.id.home_button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            finish()
+        }
     }
 }
