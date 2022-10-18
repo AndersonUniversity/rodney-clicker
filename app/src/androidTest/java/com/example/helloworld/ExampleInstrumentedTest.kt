82a16d51e48e53bfea7071e3feee4c8641975f51
jgalford <jalford@zincpartners.us>
2022-10-18 10:09:56 -0400

    test for increaseNum()

--- app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.kt
@@ -1,22 +1,18 @@
 package com.example.helloworld
 
+import android.view.View
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.helloworld", appContext.packageName)
+    fun testIncreaseNum() {
+        val view = MainActivity().findViewById<View>(R.id.ravenDollars)
+        assertEquals(1, MainActivity().increaseNum(view))
     }
 }
