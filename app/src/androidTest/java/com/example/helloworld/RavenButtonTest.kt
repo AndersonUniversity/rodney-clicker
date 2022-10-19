941c06dc5233009fc5d0c8d66b6ac677bd6ffc62
jgalford <jalford@zincpartners.us>
2022-10-19 13:15:27 -0400

    fixed RavenButtonTest

--- app/src/androidTest/java/com/example/helloworld/RavenButtonTest.kt
+++ app/src/androidTest/java/com/example/helloworld/RavenButtonTest.kt
@@ -2,6 +2,7 @@ package com.example.helloworld
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -18,6 +19,9 @@ class RavenButtonTest {
 
     @Test
     fun clickerIncrement() {
+        // Set Raven Dollars to 0
+        onView(withId(R.id.ravenDollars)).perform(replaceText("0"))
+
         // Confirm that Raven Dollars start at zero
         onView(withId(R.id.ravenDollars)).check(matches(withText("0")))
 
