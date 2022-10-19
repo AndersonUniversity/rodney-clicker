0802130e94959c471fa0f711b77cc845235bff61
jgalford <jalford@zincpartners.us>
2022-10-19 13:12:33 -0400

    added beginning of test for saving data

---
+++ app/src/androidTest/java/com/example/helloworld/SaveDataTest.kt
@@ -0,0 +1,41 @@
+package com.example.helloworld
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SaveDataTest {
+    @get:Rule
+    var rule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun saveData() {
+        //Set Raven Dollars to 0
+        onView(withId(R.id.ravenDollars)).perform(replaceText("0"))
+
+        // Confirm that Raven Dollars start at zero
+        onView(withId(R.id.ravenDollars)).check(matches(withText("0")))
+
+        // Click the button
+        onView(withId(R.id.ravenButton)).perform(doubleClick())
+
+        // Confirm that value increments
+        onView(withId(R.id.ravenDollars)).check(matches(withText("2")))
+
+        // Restart the app
+        rule.scenario.moveToState(Lifecycle.State.CREATED) //TODO this doesn't actually restart the app
+        rule.scenario.moveToState(Lifecycle.State.RESUMED)
+
+        // Check that value is saved
+        onView(withId(R.id.ravenDollars)).check(matches(withText("2")))
+    }
+}
