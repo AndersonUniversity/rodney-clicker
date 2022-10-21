4275e6a6e4fa8c1d3603767883a0b3f6e59f2df9
jgalford <91432461+jgalford@users.noreply.github.com>
2022-10-21 10:57:48 -0400

    Save and Quit Game (#16)
    Co-authored-by: jgalford <jalford@zincpartners.us>
    Co-authored-by: emmalee <emmalee.paarlberg@gmail.com>

--- app/src/androidTest/java/com/example/helloworld/SaveDataTest.kt
+++ app/src/androidTest/java/com/example/helloworld/SaveDataTest.kt
@@ -1,7 +1,7 @@
 package com.example.helloworld
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.doubleClick
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -13,12 +13,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class RavenButtonTest {
+class SaveDataTest {
     @get:Rule
     var rule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun clickerIncrement() {
+    fun saveData() {
         // Set Raven Dollars to 0
         onView(withId(R.id.ravenDollars)).perform(replaceText("0"))
 
@@ -26,13 +26,16 @@ class RavenButtonTest {
         onView(withId(R.id.ravenDollars)).check(matches(withText("0")))
 
         // Click the button
-        onView(withId(R.id.ravenButton)).perform(click())
+        onView(withId(R.id.ravenButton)).perform(doubleClick())
+        onView(withId(R.id.ravenButton)).perform(doubleClick())
 
         // Confirm that value increments
-        onView(withId(R.id.ravenDollars)).check(matches(withText("1")))
+        onView(withId(R.id.ravenDollars)).check(matches(withText("4")))
+    }
 
-        // Click again and confirm new value
-        onView(withId(R.id.ravenButton)).perform(click())
-        onView(withId(R.id.ravenDollars)).check(matches(withText("2")))
+    @Test
+    fun loadData() {
+        // Check to see if the value is saved
+        onView(withId(R.id.ravenDollars)).check(matches(withText("4")))
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.example.helloworld
+
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadData()
+    }
+
+    /**Saves all the information whenever the main activity is paused*/
+    override fun onPause() {
+        super.onPause()
+        saveData()
     }
 
     /** Called when the user taps the Store button */
@@ -22,5 +31,26 @@ class MainActivity : AppCompatActivity() {
         val ravenDollars = findViewById<EditText>(R.id.ravenDollars)
         val currNum = ravenDollars.text.toString().toInt()
         ravenDollars.setText((currNum + 1).toString())
+    }
+
+    /** Save Data */
+    private fun saveData() {
+        val currNum = findViewById<EditText>(R.id.ravenDollars).text.toString()
+
+        val sharedPref = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.apply {
+            putString("Raven_Dollars", currNum)
+        }.apply()
+    }
+
+    /** Load Data */
+    private fun loadData() {
+        val sharedPref = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedNum = sharedPref.getString("Raven_Dollars", null)
+
+        if (savedNum != null) {
+            findViewById<EditText>(R.id.ravenDollars).setText(savedNum)
+        }
     }
 }
