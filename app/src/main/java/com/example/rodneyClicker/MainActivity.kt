3774d1d5641378b67bd5ba3ae6ac321e9c953213
jdspires00 <jdspires@anderson.edu>
2022-10-25 10:20:44 -0400

    RDPS function no error

--- app/src/main/java/com/example/rodneyClicker/MainActivity.kt
+++ app/src/main/java/com/example/rodneyClicker/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -55,9 +56,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ravenDollarsPerSecond() {
-        val viewText = findViewById<EditText>(R.id.ravenDollarsPerSecond)
+        val viewText = findViewById<TextView>(R.id.ravenDollarsPerSecond)
         val currRDPS = viewText.text.toString().toInt()
-        viewText.setText("Raven Dollars Per Second: ")
+        viewText.text = ("Raven Dollars Per Second: " + (currRDPS).toString())
     }
 
     /**Game loop named render**/
