cbe09a870ec2008b413e88cf92e2da853959f781
jgalford <jalford@zincpartners.us>
2022-11-06 17:45:37 -0500

    removed extra import

--- app/src/androidTest/java/edu/anderson/rodneyClicker/BasicUpgradeTest.kt
+++ app/src/androidTest/java/edu/anderson/rodneyClicker/BasicUpgradeTest.kt
@@ -2,7 +2,6 @@ package edu.anderson.rodneyClicker
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
