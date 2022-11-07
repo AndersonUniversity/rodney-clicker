41dc95b864435989f0e8bc76f7a2bac0f5a85539
jgalford <jalford@zincpartners.us>
2022-11-06 20:29:18 -0500

    updated saveDataTest

--- app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
+++ app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
@@ -1,13 +1,15 @@
 package edu.anderson.rodneyClicker
 
+import android.content.Context
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.doubleClick
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,21 +21,36 @@ class SaveDataTest {
 
     @Test
     fun saveData() {
-        // Confirm that Raven Dollars start at zero
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$0")))
+        // Click the Rodney button 20 times
+        for (i in 1..20) {
+            onView(withId(R.id.ravenButton)).perform(ViewActions.click())
+        }
 
-        // Click the button
-        onView(withId(R.id.ravenButton)).perform(doubleClick())
-        onView(withId(R.id.ravenButton)).perform(doubleClick())
+        // Confirm that value is 20
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$20")))
 
-        // Confirm that value increments
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$4")))
+        // Open the store page
+        onView(withId(R.id.store_button)).perform(ViewActions.click())
+
+        // Buy a Rodney
+        onView(withId(R.id.buy_upgradeButton)).perform(ViewActions.click())
+
+        // Return to the main page
+        onView(withId(R.id.home_button)).perform(ViewActions.click())
 
         // Recreate activity
         rule.scenario.close()
         launch(MainActivity::class.java)
 
-        // Check to see if the value is saved
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$4")))
+        // Check that the value is stored in shared preferences
+        val targetContext = getInstrumentation().targetContext
+        val sharedPreferences = targetContext.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedRavenDollars = sharedPreferences.getString("Raven_Dollars", "0")
+        val savedRodneyClickers = sharedPreferences.getString("Rodney_Clickers", "0")
+        assert(savedRavenDollars == "10")
+        assert(savedRodneyClickers == "1")
+
+        // Check to see if the value is saved on the ui
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$10")))
     }
 }
