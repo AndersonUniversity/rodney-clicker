4e2eabbd91dcc69a55c54eb3d971bfc74fc81405
jgalford <91432461+jgalford@users.noreply.github.com>
2022-11-07 10:33:51 -0500

    Multiplier upgrade for the basic clicker (#33)
    Co-authored-by: Michael Carroll <mrcarroll@anderson.edu>
    Co-authored-by: Michael Carroll <83080699+Mrcarroll66@users.noreply.github.com>
    Co-authored-by: jgalford <jalford@zincpartners.us>
    Co-authored-by: jdspires00 <91626436+jdspires00@users.noreply.github.com>
    Co-authored-by: fuzorh <uzorhfrancis@gmail.com>

--- app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
+++ app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
@@ -1,13 +1,15 @@
 package edu.anderson.rodneyClicker
 
+import android.content.Context
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.doubleClick
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,21 +21,47 @@ class SaveDataTest {
 
     @Test
     fun saveData() {
-        // Confirm that Raven Dollars start at zero
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$0")))
+        // Click the Rodney button 115 times
+        for (i in 1..115) {
+            onView(withId(R.id.ravenButton)).perform(ViewActions.click())
+        }
 
-        // Click the button
-        onView(withId(R.id.ravenButton)).perform(doubleClick())
-        onView(withId(R.id.ravenButton)).perform(doubleClick())
+        // Confirm that value is 115
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$115")))
 
-        // Confirm that value increments
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$4")))
+        // Open the store page
+        onView(withId(R.id.store_button)).perform(ViewActions.click())
+
+        // Buy a Rodney
+        onView(withId(R.id.buy_upgradeButton)).perform(ViewActions.click())
+
+        // Buy a multiplier
+        onView(withId(R.id.buy_multiplier_upgrade)).perform(ViewActions.click())
+
+        // Return to the main page
+        onView(withId(R.id.home_button)).perform(ViewActions.click())
 
         // Recreate activity
         rule.scenario.close()
         launch(MainActivity::class.java)
 
-        // Check to see if the value is saved
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$4")))
+        // Check that the value is stored in shared preferences
+        val targetContext = getInstrumentation().targetContext
+        val sharedPreferences = targetContext.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedRavenDollars = sharedPreferences.getString("Raven_Dollars", "0")
+        val savedRodneyClickers = sharedPreferences.getString("Rodney_Clickers", "0")
+        val savedRodneyClickersMultipliers = sharedPreferences.getString("Rodney_Multipliers", "0")
+
+        assert(savedRavenDollars == "5")
+        assert(savedRodneyClickers == "1")
+        assert(savedRodneyClickersMultipliers == "2")
+
+        // Wait 1 second for ui to update
+        Thread.sleep(1000)
+
+        // Check to see if the values are saved on the ui
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$7")))
+        onView(withId(R.id.total_rodneys)).check(matches(withText("Total Rodneys: 1")))
+        onView(withId(R.id.ravenDollarsPerSecond)).check(matches(withText("Raven Dollars Per Second: 2")))
     }
 }
