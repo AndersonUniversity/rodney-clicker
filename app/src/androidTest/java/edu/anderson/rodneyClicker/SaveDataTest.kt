a467eb63907155c562d16127b1de3ac26e80fd7a
jgalford <jalford@zincpartners.us>
2022-11-06 21:38:42 -0500

    added multiplier upgrade

--- app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
+++ app/src/androidTest/java/edu/anderson/rodneyClicker/SaveDataTest.kt
@@ -21,19 +21,22 @@ class SaveDataTest {
 
     @Test
     fun saveData() {
-        // Click the Rodney button 20 times
-        for (i in 1..20) {
+        // Click the Rodney button 115 times
+        for (i in 1..115) {
             onView(withId(R.id.ravenButton)).perform(ViewActions.click())
         }
 
-        // Confirm that value is 20
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$20")))
+        // Confirm that value is 115
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$115")))
 
         // Open the store page
         onView(withId(R.id.store_button)).perform(ViewActions.click())
 
         // Buy a Rodney
         onView(withId(R.id.buy_upgradeButton)).perform(ViewActions.click())
+
+        // Buy a multiplier
+        onView(withId(R.id.buy_multiplier_upgrade)).perform(ViewActions.click())
 
         // Return to the main page
         onView(withId(R.id.home_button)).perform(ViewActions.click())
@@ -47,10 +50,18 @@ class SaveDataTest {
         val sharedPreferences = targetContext.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
         val savedRavenDollars = sharedPreferences.getString("Raven_Dollars", "0")
         val savedRodneyClickers = sharedPreferences.getString("Rodney_Clickers", "0")
-        assert(savedRavenDollars == "10")
-        assert(savedRodneyClickers == "1")
+        val savedRodneyClickersMultipliers = sharedPreferences.getString("Rodney_Multipliers", "0")
 
-        // Check to see if the value is saved on the ui
-        onView(withId(R.id.ravenDollars)).check(matches(withText("R$10")))
+        assert(savedRavenDollars == "5")
+        assert(savedRodneyClickers == "1")
+        assert(savedRodneyClickersMultipliers == "2")
+
+        // Wait 1 second for ui to update
+        Thread.sleep(1000)
+
+        // Check to see if the values are saved on the ui
+        onView(withId(R.id.ravenDollars)).check(matches(withText("R$7")))
+        onView(withId(R.id.total_rodneys)).check(matches(withText("Total Rodneys: 1")))
+        onView(withId(R.id.ravenDollarsPerSecond)).check(matches(withText("Raven Dollars Per Second: 2")))
     }
 }
